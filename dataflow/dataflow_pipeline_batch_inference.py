8a4f9ba1257c9881ac2477bf855958712d3f98ac
PraveenAdepu <56210307+PraveenAdepu@users.noreply.github.com>
2021-06-15 08:20:47 +1000

    linting (#2)
    1. black on all py files for linting
    2. tested

--- dataflow/dataflow_pipeline_batch_inference.py
+++ dataflow/dataflow_pipeline_batch_inference.py
@@ -9,10 +9,22 @@ from sklearn.ensemble import RandomForestClassifier
 
 import apache_beam as beam
 from google.cloud import storage
-from apache_beam.options.pipeline_options import (StandardOptions, GoogleCloudOptions, SetupOptions, PipelineOptions, WorkerOptions)
+from apache_beam.options.pipeline_options import (
+    StandardOptions,
+    GoogleCloudOptions,
+    SetupOptions,
+    PipelineOptions,
+    WorkerOptions,
+)
 
 # feature engineering logic
-from src.feature_engineering import (dummy_dict, internet_dict, inference_yesno_cols, internet_cols, data_transformations)
+from src.feature_engineering import (
+    dummy_dict,
+    internet_dict,
+    inference_yesno_cols,
+    internet_cols,
+    data_transformations,
+)
 
 # set project directory
 project_directory = dirname(abspath("__file__"))
@@ -29,19 +41,57 @@ PROJECT = config["parameters"]["project"]
 JOB_NAME = config["parameters"]["Dataflow_job_name"]
 REGION = config["parameters"]["region"]
 BUCKET = config["parameters"]["bucket"]
-STAGING_LOCATION = 'gs://'+config["parameters"]["bucket"]+"/"+config["parameters"]["staging_folder"]
-TEMP_LOCATION = 'gs://'+config["parameters"]["bucket"]+"/"+config["parameters"]["temp_folder"]
+STAGING_LOCATION = (
+    "gs://"
+    + config["parameters"]["bucket"]
+    + "/"
+    + config["parameters"]["staging_folder"]
+)
+TEMP_LOCATION = (
+    "gs://" + config["parameters"]["bucket"] + "/" + config["parameters"]["temp_folder"]
+)
 SERVICE_ACCOUNT_EMAIL = config["parameters"]["service_account_email"]
-RUNNER =  config["parameters"]["dataflow_runner"] 
+RUNNER = config["parameters"]["dataflow_runner"]
 SAVE_MAIN_SESSION = bool(config["parameters"]["save_main_session"])
 SETUP_FILE = config["parameters"]["setup_file"]
 NUM_WORKERS = config["parameters"]["num_workers"]
-AUTOSCALING_ALGORITHM = config["parameters"]["autoscaling_algorithm"]  
-MODEL_PATH = config["parameters"]["model_artifacts_folder"]+"/"+config["parameters"]["blob_model_artifact"]
+AUTOSCALING_ALGORITHM = config["parameters"]["autoscaling_algorithm"]
+MODEL_PATH = (
+    config["parameters"]["model_artifacts_folder"]
+    + "/"
+    + config["parameters"]["blob_model_artifact"]
+)
 MODEL_ARTIFACT = config["parameters"]["blob_model_artifact"]
-PREDICTIONS_FILE = 'gs://'+config["parameters"]["bucket"]+"/"+config["parameters"]["predictions_folder"]+"/"
+PREDICTIONS_FILE = (
+    "gs://"
+    + config["parameters"]["bucket"]
+    + "/"
+    + config["parameters"]["predictions_folder"]
+    + "/"
+)
 
-source_columns = ['customerID','gender','SeniorCitizen','Partner','Dependents','tenure','PhoneService','MultipleLines','InternetService','OnlineSecurity','OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling','PaymentMethod','MonthlyCharges','TotalCharges']
+source_columns = [
+    "customerID",
+    "gender",
+    "SeniorCitizen",
+    "Partner",
+    "Dependents",
+    "tenure",
+    "PhoneService",
+    "MultipleLines",
+    "InternetService",
+    "OnlineSecurity",
+    "OnlineBackup",
+    "DeviceProtection",
+    "TechSupport",
+    "StreamingTV",
+    "StreamingMovies",
+    "Contract",
+    "PaperlessBilling",
+    "PaymentMethod",
+    "MonthlyCharges",
+    "TotalCharges",
+]
 
 # =============================================================================
 # Build and run the pipeline
@@ -49,16 +99,21 @@ source_columns = ['customerID','gender','SeniorCitizen','Partner','Dependents','
 class FormatInput(beam.DoFn):
     def process(self, element):
         import pandas as pd
-        
+
         """ Format the input to the desired shape"""
         df = pd.DataFrame([element], columns=source_columns)
-        df = data_transformations(df, dummy_dict, internet_dict, inference_yesno_cols, internet_cols)        
+        df = data_transformations(
+            df, dummy_dict, internet_dict, inference_yesno_cols, internet_cols
+        )
         df = df.fillna(0)
-        output = df.to_dict('records')
-        return output #return a dict for easier comprehension
+        output = df.to_dict("records")
+        return output  # return a dict for easier comprehension
 
-class PredictSklearn(beam.DoFn):   
-    def __init__(self, project=None, bucket_name=None, model_path=None, destination_name=None):
+
+class PredictSklearn(beam.DoFn):
+    def __init__(
+        self, project=None, bucket_name=None, model_path=None, destination_name=None
+    ):
         self._model = None
         self._project = project
         self._bucket_name = bucket_name
@@ -66,41 +121,45 @@ class PredictSklearn(beam.DoFn):
         self._destination_name = destination_name
 
     def setup(self):
-        from google.cloud import storage        
+        from google.cloud import storage
         from joblib import load
         import pickle
         from sklearn.ensemble import RandomForestClassifier
+
         """Download sklearn model from GCS"""
-        logging.info(
-            "Sklearn model initialisation {}".format(self._model_path))
+        logging.info("Sklearn model initialisation {}".format(self._model_path))
         """
         download_blob(bucket_name=self._bucket_name, source_blob_name=self._model_path,
                       project=self._project, destination_file_name=self._destination_name)
         """
         # storage_key = "statscope-4fc1b774c9d0.json"
         # self.storage_client = storage.Client.from_service_account_json(storage_key)
-        
+
         self.storage_client = storage.Client(self._project)
         self.bucket = self.storage_client.get_bucket(self._bucket_name)
         self.blob = self.bucket.blob(self._model_path)
         self.blob.download_to_filename(self._destination_name)
         # unpickle sklearn model
         # self._model = load(self._destination_name)
-        with open(self._destination_name, 'rb') as handle:
+        with open(self._destination_name, "rb") as handle:
             self._model = pickle.load(handle)
 
     def process(self, element):
         from sklearn.ensemble import RandomForestClassifier
         import numpy as np
+
         """Inference from trained model artifact"""
-        input_dat = {k: element[k] for k in element.keys() if k not in ['customerID']}        
+        input_dat = {k: element[k] for k in element.keys() if k not in ["customerID"]}
         tmp = np.array(list(i for i in input_dat.values()))
-        tmp = tmp.reshape(1, -1)        
-        element["prediction"] = self._model.predict_proba(tmp)[:,1].item()
+        tmp = tmp.reshape(1, -1)
+        element["prediction"] = self._model.predict_proba(tmp)[:, 1].item()
         # element["prediction"] = 0
-        output = {k: element[k] for k in element.keys() if k in ['customerID', 'prediction']}
-        output['customerID'] = str(output['customerID'])
+        output = {
+            k: element[k] for k in element.keys() if k in ["customerID", "prediction"]
+        }
+        output["customerID"] = str(output["customerID"])
         return [output]
+
 
 def run(argv=None):
     pipeline_options = PipelineOptions(flags=argv)
@@ -116,34 +175,42 @@ def run(argv=None):
     pipeline_options.view_as(SetupOptions).save_main_session = SAVE_MAIN_SESSION
     pipeline_options.view_as(SetupOptions).setup_file = SETUP_FILE
     pipeline_options.view_as(WorkerOptions).num_workers = NUM_WORKERS
-    pipeline_options.view_as(WorkerOptions).autoscaling_algorithm = AUTOSCALING_ALGORITHM
-    
+    pipeline_options.view_as(
+        WorkerOptions
+    ).autoscaling_algorithm = AUTOSCALING_ALGORITHM
+
     logging.info("Pipeline arguments: {}".format(pipeline_options))
 
     # table_schema = 'customerID: STRING, prediction: FLOAT'
-    query = ("SELECT * FROM `statscope.dataflow_pipeline_batch_inference.testingSet` LIMIT 10")
+    query = "SELECT * FROM `statscope.dataflow_pipeline_batch_inference.testingSet` LIMIT 10"
     bq_source = beam.io.BigQuerySource(query=query, use_standard_sql=True)
     p = beam.Pipeline(options=pipeline_options)
-    (p
-     | "Read data from BQ" >> beam.io.Read(bq_source)     
-     | "Preprocess data" >> beam.ParDo(FormatInput())
-     | "predicting" >> beam.ParDo(
-                PredictSklearn(project=PROJECT, bucket_name=BUCKET, model_path=MODEL_PATH,
-                               destination_name=MODEL_ARTIFACT))
-#     | "Write data to BQ" >> beam.io.WriteToBigQuery(table='prediction', dataset='Telco_Churn', project='statscope',
-#                                                     schema=table_schema,
-#                                                     # create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-#                                                     write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
-     | "write to GCS" >> beam.io.WriteToText(PREDICTIONS_FILE,file_name_suffix='.csv')
-     )
+    (
+        p
+        | "Read data from BQ" >> beam.io.Read(bq_source)
+        | "Preprocess data" >> beam.ParDo(FormatInput())
+        | "predicting"
+        >> beam.ParDo(
+            PredictSklearn(
+                project=PROJECT,
+                bucket_name=BUCKET,
+                model_path=MODEL_PATH,
+                destination_name=MODEL_ARTIFACT,
+            )
+        )
+        #     | "Write data to BQ" >> beam.io.WriteToBigQuery(table='prediction', dataset='Telco_Churn', project='statscope',
+        #                                                     schema=table_schema,
+        #                                                     # create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        #                                                     write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
+        | "write to GCS"
+        >> beam.io.WriteToText(PREDICTIONS_FILE, file_name_suffix=".csv")
+    )
 
     result = p.run()
     result.wait_until_finish()
-    
+
+
 # log the output
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     run()
-
-
-    
--- dataflow/setup.py
+++ dataflow/setup.py
@@ -9,12 +9,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='telco-churn',
-    version='0.1',
-    author='Nghia',
-    author_email='nghiatrinh125@gmail.com',
-    install_requires=["numpy", "pandas", "google-cloud-storage", "scikit-learn", "joblib==0.13.2","pickle"],
-    packages=find_packages(exclude=['data']),
-    description='Telco churn prediction',
-    url=''
+    name="telco-churn",
+    version="0.1",
+    author="Nghia",
+    author_email="nghiatrinh125@gmail.com",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "google-cloud-storage",
+        "scikit-learn",
+        "joblib==0.13.2",
+        "pickle",
+    ],
+    packages=find_packages(exclude=["data"]),
+    description="Telco churn prediction",
+    url="",
 )
